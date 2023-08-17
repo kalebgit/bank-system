23f037ddd70a9889952a4749143468d3ca2fadc4
Emiliano Jiménez <emilianokaleb@gmail.com>
2023-08-16 19:19:28 -0600

    feat(): exceptions coded

---
+++ src/main/java/util/BankExceptionType.java
@@ -0,0 +1,18 @@
+package util;
+
+public enum BankExceptionType {
+	PRODUCTNOTFOUND((byte)1, "El producto no fue encontrado"), PRODUCTTYPENOTDECLARED((byte)2, 
+			"El tipo de producto no se encuentra en ninguna de nuestras listas");
+	
+	private Byte code;
+	private String message;
+	
+	private BankExceptionType(byte code, String message){
+		this.code = Byte.valueOf(code);
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return this.message;
+	}
+}
