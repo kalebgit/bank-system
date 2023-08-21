f3a0110e8ecca3c028c2aeb59a3f2280208b8d93
Emiliano Jimenez <emilianokaleb@gmail.com>
2023-08-20 21:58:31 -0600

    feat(): cud functions for debitCard

--- src/main/java/businessObjects/DebitCard.java
+++ src/main/java/businessObjects/DebitCard.java
@@ -98,40 +98,7 @@ public class DebitCard extends BankProduct {
 		return Objects.equals(cardNumber, other.cardNumber);
 	}
 
-	public boolean insert(DebitCard element, Account owner) {
-		Connection conn = getConnection();
-		String query = "INSERT INTO DebitCard VALUES (?, ?, ?, ?, ?)";
-		try {
-			PreparedStatement p = conn.prepareStatement(query);
-			p.setBigDecimal(1, element.getCardNumber());
-			p.setInt(2, element.getNip());
-			p.setDouble(3, element.getMoney());
-			p.setInt(4, element.isDefault() ? 1 : 0);
-			p.setLong(5, owner.getProductID());
-			return true;
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
-
-	@Override
-	public boolean update(DebitCard element) {
-		
-		return false;
-	}
-
-	@Override
-	public boolean delete(DebitCard element) {
-		
-		return false;
-	}
-
-	@Override
-	public boolean insert(DebitCard element) throws UnsupportedOperationException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException();
-	}
+	
 
 	
 	
--- src/main/java/dataSources/DebitCardImplementation.java
+++ src/main/java/dataSources/DebitCardImplementation.java
@@ -11,7 +11,8 @@ import dataAccessObject.DAO;
 public class DebitCardImplementation implements DAO<DebitCard, Long>, ConnectionSqlServer{
 	public boolean insert(DebitCard element, Account owner) {
 		Connection conn = getConnection();
-		String query = "INSERT INTO DebitCard VALUES (?, ?, ?, ?, ?)";
+		String query = "INSERT INTO DebitCard(Number, NIP, Funds, IsDefault, AccountID) "
+				+ " VALUES (?, ?, ?, ?, ?)";
 		try {
 			PreparedStatement p = conn.prepareStatement(query);
 			p.setBigDecimal(1, element.getCardNumber());
@@ -28,13 +29,38 @@ public class DebitCardImplementation implements DAO<DebitCard, Long>, Connection
 
 	@Override
 	public boolean update(DebitCard element) {
-		
+		Connection conn = getConnection();
+		String query = "UPDATE DebitCard SET Number=?, NIP=?, Funds=?, IsDefault=?"
+				+ "WHERE DebitCardID=? AND Number=? AND NIP=?";
+		try {
+			PreparedStatement p = conn.prepareStatement(query);
+			p.setBigDecimal(1, element.getCardNumber());
+			p.setInt(2, element.getNip());
+			p.setDouble(3, element.getMoney());
+			p.setInt(4, element.isDefault() ? 1 : 0);
+			p.setLong(5, element.getProductID());
+			p.setBigDecimal(6, element.getCardNumber());
+			p.setInt(7, element.getNip());
+			return true;
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 
 	@Override
 	public boolean delete(DebitCard element) {
-		
+		Connection conn = getConnection();
+		String query = "DELETE FROM DebitCard WHERE DebitCardID=? AND Number=? AND NIP=? ";
+		try {
+			PreparedStatement p = conn.prepareStatement(query);
+			p.setLong(1, element.getProductID());
+			p.setBigDecimal(2, element.getCardNumber());
+			p.setInt(3, element.getNip());
+			return true;
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 
